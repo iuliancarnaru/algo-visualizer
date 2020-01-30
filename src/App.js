f1bbc6edcb5b25645aa7d8f47e58af175d891e5a
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2020-01-30 21:09:33 +0000

    adding merge sort

--- src/App.js
+++ src/App.js
@@ -56,9 +56,47 @@ const App = () => {
     return arr;
   };
 
-  const mergeSort = () => {
-    console.log(`merge sort`);
+  // mergeSort is splitted in 2 (merge and mergeSort called recursively)
+
+  const merge = (arr1, arr2) => {
+    let result = [];
+
+    let i = 0;
+    let j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+      if (arr1[i] > arr2[j]) {
+        result.push(arr2[j]);
+        j++;
+      } else {
+        result.push(arr1[i]);
+        i++;
+      }
+    }
+
+    while (i < arr1.length) {
+      result.push(arr1[i]);
+      i++;
+    }
+
+    while (j < arr2.length) {
+      result.push(arr2[j]);
+      j++;
+    }
+
+    return result;
   };
+
+  const mergeSort = arr => {
+    if (arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+
+    return merge(left, right);
+  };
+
   const quickSort = () => {
     console.log(`quick sort`);
   };
