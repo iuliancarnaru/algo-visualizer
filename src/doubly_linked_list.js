d284d685eb5060a17af49a75466b5c54379f2c74
iuliancarnaru <iulian.carnaru@gmail.com>
2020-02-10 21:46:40 +0000

    adding more methods

--- src/doubly_linked_list.js
+++ src/doubly_linked_list.js
@@ -26,4 +26,36 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+    this.head = oldHead.next;
+    this.head.prev = null;
+    oldHead.next = null;
+    this.length--;
+    return oldHead;
+  }
 }
+
+let list = new DoublyLinkedList();
+list.pop();
