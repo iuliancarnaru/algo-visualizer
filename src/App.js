b74bd4e194bfd46419bc869089a3e7195e769e29
iuliancarnaru <iulian.carnaru@gmail.com>
2020-03-11 19:49:50 +0000

    fixing insertion sort bug

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const generateRandomArray = () => {
     setRandomArr(
       Array(50)
-        .fill()
+        .fill("")
         .map(() => Math.floor(Math.random() * 100) + 1)
     );
   };
@@ -48,7 +48,8 @@ const App = () => {
   const insertionSort = arr => {
     for (let i = 1; i < arr.length; i++) {
       let currentVal = arr[i];
-      for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      let j;
+      for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
         arr[j + 1] = arr[j];
       }
       arr[j + 1] = currentVal;
