e5774b6838b1353c0a45791d2e0d6c3a9d3e50c6
iuliancarnaru <iulian.carnaru@gmail.com>
2020-03-12 21:40:04 +0000

    implementing a stack

---
+++ src/stack.js
@@ -0,0 +1,43 @@
+// with arrays [push -> pop] or [shift -> unshift] (push/pop more performable)
+// singly linked list implementation
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if(!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.length;
+  }
+
+  pop() {
+    if (!this.first) return null;
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.length--;
+    return temp.val;
+  }
+}
+
+// let stack = new Stack();
