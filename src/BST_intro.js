4170e41521fa2150275450113353adf7f9f72e71
iuliancarnaru <iulian.carnaru@gmail.com>
2020-03-14 20:40:27 +0000

    adding bfs search

--- src/BST_intro.js
+++ src/BST_intro.js
@@ -54,6 +54,20 @@ class BinarySearchTree {
       }
       return false;
     }
+
+    BFS() {
+      let data = [];
+      let queue = [];
+      let node = this.root;
+      queue.push(node);
+      while (queue.length) {
+        node = queue.shift();
+        data.push(node.value);
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+      }
+      return data;
+    }
 }
 
 // let tree = new BinarySearchTree();
