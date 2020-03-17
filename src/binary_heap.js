c4ce2354e15eac27c5f8481a275836a762facefa
iuliancarnaru <iulian.carnaru@gmail.com>
2020-03-17 14:34:24 +0000

    adding removeMax method

--- src/binary_heap.js
+++ src/binary_heap.js
@@ -8,6 +8,16 @@ class MaxBinaryHeap {
     this.bubbleUp();
   }
 
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length  > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+
   bubbleUp() {
     let index = this.values.length - 1;
     const element = this.values[index];
@@ -20,7 +30,42 @@ class MaxBinaryHeap {
       index = parentIndex;
     }
   }
+
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild > element) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[index] =this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
 heap.insert(55);
+heap.extractMax();
