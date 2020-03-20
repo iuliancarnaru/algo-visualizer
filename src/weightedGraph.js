e20a29c170099cb86fbe092bb287fa12c9be5802
iuliancarnaru <iulian.carnaru@gmail.com>
2020-03-20 21:27:25 +0000

    Âadding shortest path algorithm-dijkstra update with heap

--- src/weightedGraph.js
+++ src/weightedGraph.js
@@ -1,20 +1,99 @@
 // basic priority queues
+// class PriorityQueue {
+//   constructor() {
+//     this.values = [];
+//   }
+//
+//   enqueue(val,priority) {
+//     this.values.push({ val, priority});
+//     this.sort();
+//   }
+//
+//   dequeue() {
+//     return this.values.shift();
+//   }
+//
+//   sort() {
+//     this.values.sort((a,b) => a.priority - b.priority);
+//   }
+// }
+
+// using heap priority queue
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+// if two items have same priority we can filter based on other fields
+// like time that was inserted, or other values
 class PriorityQueue {
   constructor() {
     this.values = [];
   }
 
-  enqueue(val,priority) {
-    this.values.push({ val, priority});
-    this.sort();
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
   }
 
   dequeue() {
-    return this.values.shift();
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length  > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
   }
 
-  sort() {
-    this.values.sort((a,b) => a.priority - b.priority);
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
+
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+
+      if (swap === null) break;
+      this.values[index] =this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
   }
 }
 
