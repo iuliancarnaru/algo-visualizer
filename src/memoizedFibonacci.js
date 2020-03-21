d800cac9297f4f17356f006242fce6092aed70bc
iuliancarnaru <iulian.carnaru@gmail.com>
2020-03-21 13:39:36 +0000

    memoization v2

--- src/memoizedFibonacci.js
+++ src/memoizedFibonacci.js
@@ -6,4 +6,12 @@ function memoizedFib(n, memo = []) {
   return res;
 }
 
+function memoizedFib2(n, memo = [undefined, 1 , 1]) {
+  if (memo[n] !== undefined) return memo[n];
+  let res = memoizedFib(n - 1, memo) + memoizedFib(n - 2, memo);
+  memo[n] = res;
+  return res;
+}
+
 memoizedFib(45);
+memoizedFib2(50);
